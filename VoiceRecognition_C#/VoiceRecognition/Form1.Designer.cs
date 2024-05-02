f8901d1de9373a67351d677c2ee2dc1159b889f9
Kelvin Masina <168743884+KelvinMasina@users.noreply.github.com>
2024-05-02 17:33:36 +0200

    Add files via upload

---
+++ VoiceRecognition_C#/VoiceRecognition/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace VoiceRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtContents = new System.Windows.Forms.RichTextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtContents
+            // 
+            this.txtContents.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContents.Location = new System.Drawing.Point(12, 54);
+            this.txtContents.Name = "txtContents";
+            this.txtContents.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.txtContents.Size = new System.Drawing.Size(694, 312);
+            this.txtContents.TabIndex = 0;
+            this.txtContents.Text = "";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(552, 386);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(109, 35);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(56, 386);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(109, 35);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 433);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.txtContents);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtContents;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ VoiceRecognition_C#/VoiceRecognition/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
+using System.Diagnostics;
+
+namespace VoiceRecognition
+{
+    public partial class Form1 : Form
+    {
+        //Declaring global variables and objects
+        SpeechSynthesizer ss = new SpeechSynthesizer();
+        PromptBuilder pb = new PromptBuilder();
+        SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+        Choices clist = new Choices();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            
+            //Star button click
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            clist.Add(new string[]{ "Hello", "How are you?", "Thank You", "What is the current time?",
+                "Open VLC", "Close" });
+            Grammar gr = new Grammar(new GrammarBuilder(clist)); 
+
+            try
+            {
+                sre.RequestRecognizerUpdate();
+                sre.LoadGrammar(gr);
+                sre.SpeechRecognized += sre_SpeechRecognized;
+                sre.SetInputToDefaultAudioDevice();
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");               
+            }
+
+        }
+
+        private void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            ss.SelectVoiceByHints(VoiceGender.Female);
+            switch(e.Result.Text.ToString())
+            {
+                case "Hello":
+                    ss.SpeakAsync("Hello Johnny Flag. My name is Florence, I am your personal assistant.");
+                    break;
+                case "How are you?":
+                    ss.SpeakAsync("I am doing great, How about you?");
+                    break;
+                case "Thank You":
+                    ss.SpeakAsync("You are welcome Johnny");
+                    break;
+                case "What is the current time?":
+                    ss.SpeakAsync("Current time is " + DateTime.Now.ToLongTimeString());
+                    break;
+                case "Open VLC":
+                    Process.Start("VLC"); 
+                    break;
+                case "Close":
+                    Application.Exit();
+                    break;
+            }
+            txtContents.Text += e.Result.Text.ToString() + Environment.NewLine;
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            //Stop button click
+            sre.RecognizeAsyncStop();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+    }
+}
